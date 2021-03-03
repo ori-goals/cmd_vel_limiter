2f976d6e866483710cd3be4b2529fad40172fa4c
Nick Hawes <nickh@robots.ox.ac.uk>
2021-03-03 09:40:52 +0000

    Added some notes

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # cmd_vel_limiter
-ROS package for limiting cmd_vel given a signal, e.g. from an obstacle detector
+
+ROS package for limiting cmd_vel given thresholded distance from a laser scan.
+
+## Topics
+
+Input
+ - `scan` - the LaserScan topic to listen to to compare to a distance threshold
+ - `cmd_vel` - the Twist topic to limit
+Output:
+ - `cmd_vel_limited`- the Twist topic with the limit applied
+
+## Parameters
+
+ - `distance_threshold` - The distance threshold to apply to the laser scan, default 1m.
+ - `max_x_vel` - The max x velocity to be allowed under the threshold, default 0.1 m/s.
+ - `limit_in_reverse` - Whether the limit should be applied to negative x velocities, default false.
+
+
+ # Notes
+
+ The threshold is checked at the rate the laser topic publishes at. This is likely to yield frequent changes in threshold application when noise is present near the threshold value. The simplest approach to deal with this is to use [topic_tools](https://wiki.ros.org/topic_tools) to create a throttled version of the laser topic, and use that in place of the `scan` input (via topic remapping).
+
+# Demo for Husky 
+
+If you are testing this with the default Husky launch files, the command velocity from teleop is sent to `/joy_teleop/cmd_vel` where it is read by a command multiplexor. I struggled a little to get the topics to line up using topic remapping, therefore I had to clone `husky_control` into my workspace and edit the config file for the multiplexor (`rosed husky_control twist_mux.yaml`) to replace `  topic   : joy_teleop/cmd_vel` with   `topic   : joy_teleop/cmd_vel_limited`. This makes the mutliplexor igore the original topic and listen to the limited one instead.
+
+With this in place the limiter can run with the following command:
+
+`ROS_NAMESPACE=/joy_teleop rosrun cmd_vel_limiter laser_threshold scan:=/scan`
+
+Note I push the node into the `joy_teleop` so that the command vel topics line up, but this requires that the relative topic name `scan` be made absolute.
+
+
+
+
+
+
+ ROS_NAMESPACE=/joy_teleop rosrun cmd_vel_limiter laser_threshold scan:=/scan
+
+
+
--- src/laser_threshold.cpp
+++ src/laser_threshold.cpp
@@ -2,12 +2,17 @@
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Twist.h"
 
+// Globals which are set by rosparam, do not edit here
+
 // any range value less than this will trigger speed reduction
-double throttle_distance;
-//value to cap x vel to when throttle_distance is violated
+double distance_threshold;
+//value to cap x vel to when distance_threshold is violated
 double max_x_vel;
 // whether the distrance threshold has been violated
-bool apply_throttle = false;
+bool apply_limit = false;
+// whether the distrance threshold has been violated
+bool limit_in_reverse = false;
+
 
 // topic for publishing thresholded commands
 ros::Publisher cmd_vel_pub;
@@ -15,25 +20,25 @@ ros::Publisher cmd_vel_pub;
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
   for(auto i = msg->ranges.begin(); i != msg->ranges.end(); ++i) {
-    if(*i < throttle_distance) {
-      ROS_DEBUG_STREAM("Threshold under: " << *i);
-      apply_throttle = true;
+    if(*i < distance_threshold) {
+      ROS_DEBUG_STREAM("Value under threshold: " << *i);
+      apply_limit = true;
       return;
     }
   }
-  apply_throttle = false;
+  apply_limit = false;
 }
 
 void cmdCallback(const geometry_msgs::Twist::Ptr& msg)
 {
 
-  if(apply_throttle) {
+  if(apply_limit) {
     // apply limit
     if(msg->linear.x > max_x_vel) {
       ROS_DEBUG_STREAM("Applying limit forwards");
       msg->linear.x = max_x_vel;
     }
-    else if(msg->linear.x < -max_x_vel) {
+    else if(limit_in_reverse && msg->linear.x < -max_x_vel) {
       ROS_DEBUG_STREAM("Applying limit backwards");
       msg->linear.x = -max_x_vel;
     }
@@ -46,15 +51,17 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "laser_threshold");
   ros::NodeHandle n;
-  n.param("throttle_distance", throttle_distance, 1.0);
+  n.param("distance_threshold", distance_threshold, 1.0);
   n.param("max_x_vel", max_x_vel, 0.1);
+  n.param("limit_in_reverse", limit_in_reverse, false);
 
-  ROS_INFO_STREAM("Throttling under distance: " << throttle_distance << "m");
+  ROS_INFO_STREAM("Throttling under distance: " << distance_threshold << "m");
   ROS_INFO_STREAM("Throttling to max x vel: " << max_x_vel << "m/s");
+  ROS_INFO_STREAM("Limiting in reverse: " << limit_in_reverse);
 
   ros::Subscriber scan_sub = n.subscribe("scan", 10, laserCallback);
   ros::Subscriber cmd_sub = n.subscribe("cmd_vel", 10, cmdCallback);
-  cmd_vel_pub = n.advertise<geometry_msgs::Twist>("max_x_vel", 10);
+  cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel_limited", 10);
   ros::spin();
 
   return 0;
